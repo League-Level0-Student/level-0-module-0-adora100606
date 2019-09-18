ef695205ccf7c2f4d14cfa28f86d56d25b25f1dd
Adora Smith <adora100606@jointheleague.org>
2019-09-17 17:53:37 -0700

    Robot

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -13,20 +13,22 @@ public class RobotSquare {
  
     	// 1. Make a new Robot
 Robot adora=new Robot( );
-
+adora.setSpeed(100);
         // 3. Put the robot's pen down
 adora.penDown();
         // 6. Make the robot move as fast as possible
 
 
         // 5. Do everything below here 4 times
-
-
+int numsides=0;
+while( numsides<4  ) {
         //         2. Move your robot 200 pixels
 adora.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
+adora. turn (90 );
+numsides= 1+ numsides;
 
-
+}
     }
 }
---
+++ src/_02_robot_graffiti/RobotGraffiti.java
@@ -0,0 +1,13 @@
+package _02_robot_graffiti;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+public static void main(String[] args) {
+	
+	Robot Adora=new Robot( );
+	
+	
+	
+}
+}
