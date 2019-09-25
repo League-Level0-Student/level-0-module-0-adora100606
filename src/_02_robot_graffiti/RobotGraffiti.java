066498cb534ea9e8331573260e72dc604862c142
adora <i-labuser@SDPL88535-iMacProLO.local>
2019-09-24 17:51:40 -0700

    Update RobotGraffiti.java
    RobotGraffti.java

--- src/_02_robot_graffiti/RobotGraffiti.java
+++ src/_02_robot_graffiti/RobotGraffiti.java
@@ -5,9 +5,20 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotGraffiti {
 public static void main(String[] args) {
 	
-	Robot Adora=new Robot( );
+	Robot Adora=new Robot("mini");
+	Adora.setSpeed( 100);
+	Adora.penDown();
+	Adora.move(25);	
 	
+	Adora.turn(180);
+
 	
+	Adora.turn(60);
+	Adora.move(20);
 	
+
+
 }
+
+
 }
