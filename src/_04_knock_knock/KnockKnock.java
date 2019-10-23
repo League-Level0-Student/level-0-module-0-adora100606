501076fe3bdde2adadd5c938513c2faa6be7f052
i-lab user <i-labuser@SDPL88535-iMacProLO.local>
2019-10-22 17:54:02 -0700

    woringk greeter

---
+++ src/_04_knock_knock/KnockKnock.java
@@ -0,0 +1,17 @@
+package _04_knock_knock;
+
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	JOptionPane.showMessageDialog(null,"KnocKnocK");
+	JOptionPane.showMessageDialog(null,"whos there");
+	JOptionPane.showMessageDialog(null,"Europe");
+		JOptionPane.showMessageDialog(null,"Europe who");
+		JOptionPane.showMessageDialog(null,"NOiam not");
+	}
+
+}
---
+++ src/adora.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class adora {
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"bo waut to a nok jok");
+		JOptionPane.showMessageDialog(null,"got up in the moiming and befis");
+		JOptionPane.showMessageDialog(null,"bush my tef");
+		JOptionPane.showMessageDialog(null,"go to scoohl and Homework");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
