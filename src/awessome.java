1866146d4e89e0ed77e21a0282367b0575c6689c
i-lab user <i-labuser@SDPL88535-iMacProLO.local>
2019-10-29 17:56:10 -0700

    i work on awssssome

---
+++ src/awessome.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class awessome {
+public static void main(String[] args) {
+String liquid =	JOptionPane.showInputDialog( null,"what do you like?   ");
+JOptionPane.showMessageDialog(null,liquid+" is awesssome");
+} 
+}
